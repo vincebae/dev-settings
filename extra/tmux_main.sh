e53d328b01363755afc27fc0bda99fd31485d26a
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2023-05-14 21:51:28 -0700

    Update tmux_main.sh

--- extra/tmux_main.sh
+++ extra/tmux_main.sh
@@ -5,9 +5,9 @@ tmux has-session -t $session_name
 if [ "$?" -eq "0" ]; then
     tmux a -t $session_name
 else
-    tmux new-session -s $session_name -n "first" -d \; split-window -h \; select-pane -t 1
-    tmux new-window -n "second" \; split-window -h \; select-pane -t 1
-    tmux new-window -n "third" \; split-window -h \; select-pane -t 1
+    tmux new-session -s $session_name -n "first" -d
+    tmux new-window -n "second"
+    tmux new-window -n "third"
     tmux select-window -t "first"
     tmux attach-session -t $session_name
 fi
