03cbecd6cba4c2c80f082d8d5a3a2627e65b4839
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2023-05-14 21:46:28 -0700

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 local keymap = vim.keymap
 local cmd = vim.cmd
-
+local my_funs = require("config/functions")
 
 -- Search and scroll being kept in center.
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
-keymap.set("n", "<C-f>", "<C-f>zz");
-keymap.set("n", "<C-b>", "<C-b>zz");
+keymap.set("n", "<C-f>", "<C-f>zz")
+keymap.set("n", "<C-b>", "<C-b>zz")
 
 -- Move block
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -27,9 +27,11 @@ keymap.set("n", "<C-k>", "<C-W>k")
 keymap.set("n", "<C-\\>", "<C-W>_<C-W>|")
 keymap.set("n", "<C-v>", "<cmd>vsp<cr>")
 keymap.set("n", "<C-x>", "<cmd>sp<cr>")
+keymap.set("n", "<leader>T", "<cmd>vertical bo split +terminal<cr>")
 
 -- Buffer
-keymap.set("n", "?", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
+keymap.set("n", "<leader>?", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
+keymap.set("n", "<leader>bs", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
 keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>bp", "<cmd>bp<cr>")
 keymap.set("n", "<leader>bn", "<cmd>bn<cr>")
@@ -44,13 +46,17 @@ keymap.set("n", "<leader>wc", "<cmd>tabclose<cr>")
 keymap.set("n", "<leader>wo", "<cmd>tabonly<cr>")
 
 -- Cheat sheet
-local my_funs = require('config/functions')
-keymap.set("n", "<leader>cc", my_funs.open_cheatsheet("tabe", ""))
-keymap.set("n", "<leader>ch", my_funs.open_cheatsheet("belowright new", ""))
-keymap.set("n", "<leader>cv", my_funs.open_cheatsheet("belowright vnew", ""))
-keymap.set("n", "<leader>cjc", my_funs.open_cheatsheet("tabe", "java"))
-keymap.set("n", "<leader>cjh", my_funs.open_cheatsheet("belowright new", "java"))
-keymap.set("n", "<leader>cjv", my_funs.open_cheatsheet("belowright vnew", "java"))
+local open_cheatsheet_wrapper = function(command, lang)
+    return function()
+        my_funs.open_cheatsheet(command, lang)
+    end
+end
+keymap.set("n", "<leader>cc", open_cheatsheet_wrapper("tabe", ""))
+keymap.set("n", "<leader>ch", open_cheatsheet_wrapper("belowright new", ""))
+keymap.set("n", "<leader>cv", open_cheatsheet_wrapper("belowright vnew", ""))
+keymap.set("n", "<leader>cjc", open_cheatsheet_wrapper("tabe", "java"))
+keymap.set("n", "<leader>cjh", open_cheatsheet_wrapper("belowright new", "java"))
+keymap.set("n", "<leader>cjv", open_cheatsheet_wrapper("belowright vnew", "java"))
 
 -- Key maps for terminal split window
 keymap.set("t", "<C-w>", "<C-\\><C-N><C-w>")
@@ -72,3 +78,20 @@ cmd("cnoreabbrev WQ wq")
 cmd("cnoreabbrev W w")
 cmd("cnoreabbrev Q q")
 cmd("cnoreabbrev Qall qall")
+
+-- Navigation
+vim.api.nvim_create_user_command("Totop", my_funs.cd_to_top_dir, {})
+vim.api.nvim_create_user_command("Totest", my_funs.cd_to_test_dir, {})
+vim.api.nvim_create_user_command("Tomain", my_funs.cd_to_main_dir, {})
+vim.api.nvim_create_user_command("Tocurr", my_funs.cd_to_curr_dir, {})
+vim.api.nvim_create_user_command("Todir", my_funs.cd_to_dir, {})
+vim.api.nvim_create_user_command("Etest", my_funs.open_test_file, {})
+vim.api.nvim_create_user_command("Emain", my_funs.open_main_file, {})
+
+keymap.set("n", "<leader>nP", "<cmd>Totop<cr>")
+keymap.set("n", "<leader>nT", "<cmd>Totest<cr>")
+keymap.set("n", "<leader>nM", "<cmd>Tomain<cr>")
+keymap.set("n", "<leader>nt", "<cmd>Etest<cr>")
+keymap.set("n", "<leader>nm", "<cmd>Emain<cr>")
+keymap.set("n", "<leader>nC", "<cmd>Tocurr<cr>")
+keymap.set("n", "<leader>nD", "<cmd>Todir<cr>")
