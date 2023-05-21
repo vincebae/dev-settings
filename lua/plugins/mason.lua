22d69cce0a0dd602e38160ee907fe8a940dc33fd
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2023-05-20 21:26:27 -0700

    Update mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,12 +6,15 @@ return {
     opts = {
         ensure_installed = {
             "bash-language-server",
+            "black",
             "eslint_d",
             "eslint-lsp",
+            "flake8",
             "google-java-format",
             "haskell-language-server",
             "jdtls",
             "lua-language-server",
+            "pyright",
             "rust-analyzer",
             "rustfmt",
             "stylua",
