6fcb1ddfd609f22f740e08e73ac13a2ad530c948
Seung-Bin Bae <vincebae@gmail.com>
2023-06-10 12:47:22 -0700

    ...

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -17,15 +17,13 @@ metals_config.settings = {
 -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>lD", vim.lsp.buf.definition)
-vim.keymap.set("n", "<leader>lR", vim.lsp.buf.references)
-
-vim.keymap.set("n", "<leader>ldo", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>ldn", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>ldp", vim.diagnostic.goto_prev)
+-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
+-- vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
+-- vim.keymap.set("n", "<leader>lD", vim.lsp.buf.definition)
+-- vim.keymap.set("n", "<leader>lR", vim.lsp.buf.references)
+-- vim.keymap.set("n", "<leader>ldo", vim.diagnostic.open_float)
+-- vim.keymap.set("n", "<leader>ldn", vim.diagnostic.goto_next)
+-- vim.keymap.set("n", "<leader>ldp", vim.diagnostic.goto_prev)
 
 metals.initialize_or_attach(metals_config)
--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,33 +1,33 @@
 return {
-	-- "rmagatti/session-lens",
-	-- dependencies = {
-	-- 	"nvim-telescope/telescope.nvim",
-	-- 	{
-	-- 		"rmagatti/auto-session",
-	-- 		opts = {
-	-- 			log_level = "error",
-	-- 			auto_session_suppress_dirs = {
-	-- 				"~/",
-	-- 				"~/Projects",
-	-- 				"~/Downloads",
-	-- 				"/",
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	-- config = function()
-	-- 	local session_lens = require("session-lens")
-	-- 	session_lens.setup({
-	-- 		path_display = { "shorten" },
-	-- 	})
-	--
-	-- 	require("telescope").load_extension("session-lens")
-	--
-	-- 	local keymap = vim.keymap
-	--
-	-- 	keymap.set("n", "<leader>ss", "<cmd>SaveSession<cr>")
-	-- 	keymap.set("n", "<leader>sr", "<cmd>RestoreSession<cr>")
-	-- 	keymap.set("n", "<leader>sd", "<cmd>Autosession delete<cr>")
-	-- 	keymap.set("n", "<leader>sf", session_lens.search_session)
-	-- end,
+	"rmagatti/session-lens",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		{
+			"rmagatti/auto-session",
+			opts = {
+				log_level = "error",
+				auto_session_suppress_dirs = {
+					"~/",
+					"~/Projects",
+					"~/Downloads",
+					"/",
+				},
+			},
+		},
+	},
+	config = function()
+		local session_lens = require("session-lens")
+		session_lens.setup({
+			path_display = { "shorten" },
+		})
+
+		require("telescope").load_extension("session-lens")
+
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>ss", "<cmd>SaveSession<cr>")
+		keymap.set("n", "<leader>sr", "<cmd>RestoreSession<cr>")
+		keymap.set("n", "<leader>sd", "<cmd>Autosession delete<cr>")
+		keymap.set("n", "<leader>sf", session_lens.search_session)
+	end,
 }
