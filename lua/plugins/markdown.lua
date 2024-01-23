5b694cdf28912e9c29f41b8226c6d7ebb673a5cc
Seung-Bin Bae <vincebae@gmail.com>
2024-01-22 21:40:49 -0800

    Add gist

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,14 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    init = function()
-        -- vim.cmd("call mkdp#util#install()")
-    end,
+	{
+		"iamcco/markdown-preview.nvim",
+		init = function()
+			-- vim.cmd("call mkdp#util#install()")
+		end,
+	},
+	{
+		"richardbizik/nvim-toc",
+		opts = {
+			toc_header = "Table of Contents",
+		},
+	},
 }
