958c6b1c3d25437fd5c8d11636f9a31eae7c7adc
Seung-Bin Bae <vincebae@gmail.com>
2024-02-05 11:00:02 -0800

    Update tmux conf

--- extra/main_tmux.sh
+++ extra/main_tmux.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 session_name="sbbae"
-split_window_with_resize="split-window -h -d -l 67%"
-tmux has-session -t $session_name
-if [ "$?" -eq "0" ]; then
+if tmux has-session -t $session_name; then
+    echo "Attaching to existing session..."
     tmux a -t $session_name
 else
-    tmux new-session -s $session_name -n "first" -d # \; ${split_window_with_resize}
-    tmux new-window -n "second" # \; ${split_window_with_resize}
-    tmux new-window -n "third" # \; ${split_window_with_resize}
-    tmux new-window -n "fourth" # \; ${split_window_with_resize}
-    tmux select-window -t "first"
+    echo "Starting a new session..."
+    tmux new-session -s $session_name -n "main" -d -c "$HOME" \; \
+        split-window -h -d -c "$HOME"
+    tmux new-window -n "code" -c "$HOME/code" \; \
+        split-window -h -d -c "$HOME/code"
+    tmux new-window -n "bin" -c "$HOME/bin" \; \
+        split-window -h -d -c "$HOME/bin"
+    tmux new-window -n "nvim" -c "$HOME/.config/nvim" \; \
+        split-window -h -d -c "$HOME/.config/nvim"
+    tmux select-window -t "main"
     tmux attach-session -t $session_name
 fi
