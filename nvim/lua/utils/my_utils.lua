477554c2043ffff950131eca490e8b67fd24c81d
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2025-05-27 14:54:02 -0700

    Update my_utils.lua

--- nvim/lua/utils/my_utils.lua
+++ nvim/lua/utils/my_utils.lua
@@ -8,4 +8,20 @@ M.ends_with = function(str, suffix)
     return #str >= #suffix and string.sub(str, -#suffix) == suffix
 end
 
+M.make_literal = function(str, opts)
+    local result = ""
+    for c in string.gmatch(str, ".") do
+        if c == [[\]] or c == [["]] then
+            result = result .. [[\]]
+        end
+        result = result .. c
+    end
+
+    opts = opts or {}
+    if opts.newline then
+        result = result .. "\\n"
+    end
+    return [["]] .. result .. [["]]
+end
+
 return M
