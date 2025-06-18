6438d888848506e16526619e43ec66e82817d69e
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2025-06-18 16:58:55 -0700

    Update dap.lua

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -10,47 +10,47 @@ return {
 
             vim.keymap.set("n", "<leader>db", function()
                 dap.toggle_breakpoint()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Breakpoint" })
 
             vim.keymap.set("n", "<leader>dc", function()
                 dap.continue(false)
-            end, { noremap = true })
+            end, { noremap = true, desc = "Continue" })
 
             vim.keymap.set("n", "<leader>do", function()
                 dap.step_over()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Step Over" })
 
             vim.keymap.set("n", "<leader>di", function()
                 dap.step_into()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Step Into" })
 
             vim.keymap.set("n", "<leader>dt", function()
                 dap.step_out()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Step Out" })
 
             vim.keymap.set("n", "<leader>dr", function()
                 dap.repl.open()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Open Repl" })
 
             vim.keymap.set("n", "<leader>dw", function()
                 widgets.hover()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Show Widget" })
 
             vim.keymap.set("n", "<leader>ds", function()
                 widgets.centered_float(widgets.scopes)
-            end, { noremap = true })
+            end, { noremap = true, desc = "Show Scope" })
 
             vim.keymap.set("n", "<leader>df", function()
                 widgets.centered_float(widgets.frames)
-            end, { noremap = true })
+            end, { noremap = true, desc = "Show Frame" })
 
             vim.keymap.set("n", "<leader>du", function()
                 require("dapui").toggle()
-            end, { noremap = true })
+            end, { noremap = true, desc = "Toggle DAP UI" })
 
             vim.keymap.set("n", "<leader>dl", function()
                 require("osv").launch({ port = 8086 })
-            end, { noremap = true })
+            end, { noremap = true, desc = "Start Lua Debug Server" })
 
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "dap-float",
@@ -95,6 +95,6 @@ return {
             "mfussenegger/nvim-dap",
             "nvim-neotest/nvim-nio",
         },
-        opts = {}
+        opts = {},
     },
 }
