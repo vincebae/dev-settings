748b7d205b8c01a814ebb217deb89de0f43534d2
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2025-06-18 16:55:35 -0700

    Update which_key.lua

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -167,3 +167,44 @@ return {
         })
     end,
 }
+
+-- return {
+--     "folke/which-key.nvim",
+--     config = function()
+--         local which_key = require("which-key")
+--         local setup = {
+--             preset = "helix",
+--             triggers = {
+--                 { "<leader>", mode = { "n", "v" } },
+--                 { "<localleader>", mode = { "n", "v" } },
+--                 { "=", mode = { "n" } },
+--                 { "[", mode = { "n" } },
+--                 { "]", mode = { "n" } },
+--                 { "<", mode = { "n" } },
+--                 { ">", mode = { "n" } },
+--                 { "c", mode = { "n" } },
+--                 { "d", mode = { "n" } },
+--                 { "g", mode = { "n" } },
+--                 { "y", mode = { "n" } },
+--                 { "z", mode = { "n", "v" } },
+--                 { "<C-t>", mode = { "n", "v" } },
+--                 { "<C-w>", mode = { "n" } },
+--             },
+--         }
+--         which_key.setup(setup)
+-- 
+--         which_key.add({
+--             { "<C-t>", group = "Terminal" },
+--             { "<leader>b", group = "Buffer" },
+--             { "<leader>d", group = "DAP" },
+--             {
+--                 mode = { "n", "v" },
+--                 { "<leader>g", group = "Git" },
+--             },
+--             { "<leader>p", group = "Paste" },
+--             { "<leader>P", group = "Plugins" },
+--             { "<leader>s", group = "Snacks" },
+--             { "<localleader><localleader>", group = "Slime" },
+--         })
+--     end,
+-- }
