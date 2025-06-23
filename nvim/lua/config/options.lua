3f3af2b1908563ad9e41481d31049b5382ed4c8f
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2025-06-23 16:49:34 -0700

    Replace sexp with paredit

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -73,5 +73,5 @@ vim.opt.wildignore = "*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.x
 -- Misc
 vim.opt.updatetime = 50
 vim.opt.timeout = true
-vim.opt.timeoutlen = 500
+vim.opt.timeoutlen = 750
 -- vim.opt.ttimeoutlen = 0
