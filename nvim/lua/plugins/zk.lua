34b6aa33aa9648b9b76d6c0a198dce869f0728f3
Seung-Bin Bae <32313839+vincebae@users.noreply.github.com>
2026-03-17 14:32:00 -0700

    Update zk.lua

--- nvim/lua/plugins/zk.lua
+++ nvim/lua/plugins/zk.lua
@@ -5,24 +5,73 @@ return {
         keys = {
             { "<leader>zb", "<cmd>ZkBuffers<cr>", desc = "Zk Buffers" },
             { "<leader>zz", "<cmd>ZkNotes<cr>", desc = "Zk Notes" },
-            { "<leader>zt", "<cmd>ZkTags<cr>", desc = "Zk Keywords / Tags" },
-            { "<leader>zk", "<cmd>ZkTags<cr>", desc = "Zk Keywords / Tags" },
+            { "<leader>zk", "<cmd>ZkTags<cr>", desc = "Zk Keywords / Tags (Simple)" },
+            {
+                "<leader>zK",
+                function()
+                    local su = require("utils.string_utils")
+
+                    local function trim(s)
+                        return (s:gsub("^%s*(.-)%s*$", "%1"))
+                    end
+
+                    local function process_string(input_str)
+                        local items = {}
+                        -- 1. Split the string into a table using string.gmatch, accounting for optional spaces around the comma
+                        -- The pattern "[^,]+" matches one or more characters that are not a comma
+                        for item in string.gmatch(input_str, "([^,]+)") do
+                            -- 2. Trim whitespaces from each item and add it to the table
+                            local trimmed_item = trim(item)
+                            table.insert(items, trimmed_item)
+                        end
+
+                        -- 3. Wrap each item with double quotes
+                        for i, v in ipairs(items) do
+                            -- Use string concatenation ".." to add quotes
+                            items[i] = su.make_literal(v)
+                        end
+
+                        -- 4. Join the items using a comma as a delimiter
+                        local result = table.concat(items, ", ")
+                        return result
+                    end
+
+                    local text = vim.fn.input("Zk Keywords / Tags: ")
+                    if text and text ~= "" then
+                        local opts = "{ tags = { " .. process_string(text) .. " } }"
+                        vim.cmd("ZkNotes " .. opts)
+                    end
+                end,
+                desc = "Zk Keywords / Tags (Full)",
+            },
+            {
+                "<leader>zN",
+                function()
+                    local su = require("utils.string_utils")
+                    local text = vim.fn.input("Zk New (Root) Note Title: ")
+                    if text and text ~= "" then
+                        local opts = "{ title = " .. su.make_literal(text) .. "}"
+                        vim.cmd("ZkNew " .. opts)
+                    end
+                end,
+                desc = "Zk New (Root Dir)",
+            },
             {
                 "<leader>zn",
                 function()
                     local su = require("utils.string_utils")
-                    local text = vim.fn.input("Zk Title: ")
+                    local text = vim.fn.input("Zk New (Curr) Note Title: ")
                     if text and text ~= "" then
                         local pu = require("utils.path_utils")
                         local opts = "{ title = "
                             .. su.make_literal(text)
                             .. ", dir = "
                             .. su.make_literal(pu.get_buffer_dir_path())
-                            .. " }"
+                            .. "}"
                         vim.cmd("ZkNew " .. opts)
                     end
                 end,
-                desc = "Zk New",
+                desc = "Zk New (Curr Dir)",
             },
         },
         config = function()
